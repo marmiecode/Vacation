af1aab205d536e242cd71daaf2b0413ceb65d660
Martyna <mieczkowska.maartyna@gmail.com>
2024-09-09 21:26:01 +0200

    Initial Commit

---
+++ Vacation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vacation
+//
+//  Created by Martyna Mieczkowska on 09/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vacation/VacationApp.swift
@@ -0,0 +1,17 @@
+//
+//  VacationApp.swift
+//  Vacation
+//
+//  Created by Martyna Mieczkowska on 09/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct VacationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
