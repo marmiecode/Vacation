82f1fb184d42bde906bd4991a8449cdd4fb6eab8
Martyna <mieczkowska.maartyna@gmail.com>
2024-09-11 23:18:45 +0200

    A reservation button was created

---
+++ Vacation/Views/ReserveButton.swift
@@ -0,0 +1,29 @@
+//
+//  CustomButton.swift
+//  Vacation
+//
+//  Created by Martyna Mieczkowska on 11/09/2024.
+//
+
+import SwiftUI
+
+struct ReserveButton: View {
+    var action: () -> Void
+    var body: some View {
+        Button(action: action) {
+            Text("Reserve")
+                .bold()
+                .frame(minWidth: 200, maxWidth: 50)
+                .foregroundColor(.black)
+                .background(LinearGradient(gradient: Gradient(colors: [Color.pink, Color.white]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/))
+                .cornerRadius(10)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ReserveButton(action: {
+        print("Kliknity")
+    })
+}
