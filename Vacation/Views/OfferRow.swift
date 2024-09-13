13888964d10be0116af304954646ebf4fb2ff52a
Martyna <mieczkowska.maartyna@gmail.com>
2024-09-13 20:10:35 +0200

    A view of offer row has been added

---
+++ Vacation/Views/OfferRow.swift
@@ -0,0 +1,72 @@
+//
+//  OfferRow.swift
+//  Vacation
+//
+//  Created by Martyna Mieczkowska on 13/09/2024.
+//
+
+import SwiftUI
+
+struct OfferRow: View {
+    var offer: Offer
+    @State var isLiked: Bool
+    var toggleLiked: () -> Void
+    
+    
+    var body: some View {
+        HStack {
+            ZStack(alignment: .topLeading) {
+                TabView {
+                    ForEach(offer.image, id: \.self) {
+                        imageName in Image(imageName)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 200, height: 200)
+                            .cornerRadius(10)
+                    }
+                }
+                .frame(width: 150, height: 150)
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+                
+                LikeButton(isLiked: $isLiked)
+                    .padding([.top, .trailing], 8)
+            }
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text(offer.hotelName)
+                    .bold()
+                    .font(.headline)
+                
+                Text("\(offer.city), \(offer.country)")
+                    .bold()
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                    .lineLimit(1)
+                
+                Text(offer.description.prefix(30) + "...")
+                    .font(.caption)
+                
+                Text("\(offer.price, specifier: "%.2f") $ / week")
+                    .bold()
+                    .font(.subheadline)
+            }
+            .padding(.leading, 10)
+        }
+        .padding()
+    }
+}
+
+
+struct OfferRow_Previews: PreviewProvider {
+    @State static var isLiked = false
+    
+    static var previews: some View {
+        OfferRow(
+            offer: Offer(hotelName: "Berlin Hotel", city: "Berlin", country: "DE", description: "okkkkkkkkkkkkkkkkkkkkk", price: 800.50, image: ["zdj1", "zdj2"]),
+            isLiked: isLiked,
+            toggleLiked: {
+                isLiked.toggle()
+            }
+        )
+    }
+}
