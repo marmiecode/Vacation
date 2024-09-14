490bccedae38a3eda17c958f6f8d73dcd420f206
Martyna <mieczkowska.maartyna@gmail.com>
2024-09-14 02:14:30 +0200

    Added offer list view

---
+++ Vacation/Views/OfferListView.swift
@@ -0,0 +1,31 @@
+//
+//  OfferListView.swift
+//  Vacation
+//
+//  Created by Martyna Mieczkowska on 14/09/2024.
+//
+
+import SwiftUI
+
+struct OfferListView: View {
+    @ObservedObject var viewModel: OfferViewModel
+
+    var body: some View {
+        List(viewModel.offers) { offer in
+            OfferRow(
+                offer: offer,
+                isLiked: viewModel.likedOffers.contains(offer.id),
+                toggleLiked: {
+                    if viewModel.likedOffers.contains(offer.id) {
+                        viewModel.unlikeOffer(offer)
+                    } else {
+                        viewModel.likeOffer(offer)
+                    }
+                }
+            )
+        }
+    }
+}
+
+
+
