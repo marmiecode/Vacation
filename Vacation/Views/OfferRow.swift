2828b542d042dfe2f2ee5ba7553a5a66734a310e
Martyna <mieczkowska.maartyna@gmail.com>
2024-09-14 17:50:19 +0200

    Was crated temporaryBinding

--- Vacation/Views/OfferRow.swift
+++ Vacation/Views/OfferRow.swift
@@ -8,52 +8,60 @@
 import SwiftUI
 
 struct OfferRow: View {
-    var offer: Offer
-    @State var isLiked: Bool
-    var toggleLiked: () -> Void
-    
-    
-    var body: some View {
-        HStack {
-            ZStack(alignment: .topLeading) {
-                TabView {
-                    ForEach(offer.image, id: \.self) {
-                        imageName in Image(imageName)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 200, height: 200)
-                            .cornerRadius(10)
-                    }
-                }
-                .frame(width: 150, height: 150)
-                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
-                
-                LikeButton(isLiked: $isLiked)
-                    .padding([.top, .trailing], 8)
-            }
-            
-            VStack(alignment: .leading, spacing: 8) {
-                Text(offer.hotelName)
-                    .bold()
-                    .font(.headline)
-                
-                Text("\(offer.city), \(offer.country)")
-                    .bold()
-                    .font(.subheadline)
-                    .foregroundColor(.gray)
-                    .lineLimit(1)
-                
-                Text(offer.description.prefix(30) + "...")
-                    .font(.caption)
-                
-                Text("\(offer.price, specifier: "%.2f") $ / week")
-                    .bold()
-                    .font(.subheadline)
-            }
-            .padding(.leading, 10)
+  var offer: Offer
+  @State var isLiked: Bool
+  var toggleLiked: () -> Void
+  
+  
+  var body: some View {
+    HStack {
+      ZStack(alignment: .topLeading) {
+        TabView {
+          ForEach(offer.image, id: \.self) {
+            imageName in Image(imageName)
+              .resizable()
+              .scaledToFit()
+              .frame(width: 200, height: 200)
+              .cornerRadius(10)
+          }
         }
-        .padding()
+        .frame(width: 150, height: 150)
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+        
+        Button(action: {
+          toggleLiked()  
+        }) {
+          LikeButton(isLiked: Binding(
+            get: { isLiked },
+            set: { _ in toggleLiked() }
+          ))
+          .padding([.top, .trailing], 8)
+        }
+      }
+      
+      
+      VStack(alignment: .leading, spacing: 8) {
+        Text(offer.hotelName)
+          .bold()
+          .font(.headline)
+        
+        Text("\(offer.city), \(offer.country)")
+          .bold()
+          .font(.subheadline)
+          .foregroundColor(.gray)
+          .lineLimit(1)
+        
+        Text(offer.description.prefix(30) + "...")
+          .font(.caption)
+        
+        Text("\(offer.price, specifier: "%.2f") $ / week")
+          .bold()
+          .font(.subheadline)
+      }
+      .padding(.leading, 10)
     }
+    .padding()
+  }
 }
 
 
